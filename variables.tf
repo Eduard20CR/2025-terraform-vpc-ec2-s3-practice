bdef81a6dccb42e9352d93bfafb2d06b1faa9e83
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2025-12-16 19:36:42 -0600

    Refactor VPC and subnet configurations, add variables for CIDR blocks and availability zones

---
+++ variables.tf
@@ -0,0 +1,25 @@
+
+
+variable "availability_zones" {
+  description = "List of availability zones to use for the subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "cidr_block" {
+  description = "The CIDR block for the VPC"
+  type        = string
+  default     = "10.35.0.0/16"
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.35.0.0/18", "10.35.64.0/18"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.35.128.0/18", "10.35.192.0/18"]
+}
--- vpc.tf
+++ vpc.tf
@@ -1,38 +1,45 @@
-# VPC
-
 resource "aws_vpc" "main" {
-  cidr_block = "10.35.0.0/16"
+  cidr_block                       = "10.35.0.0/16"
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
 }
 
-# SUBNETS
-
 resource "aws_subnet" "private_data_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.35.0.0/18"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[0]
+  availability_zone = var.availability_zones[0]
 }
 
 resource "aws_subnet" "private_data_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.35.64.0/18"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[1]
+  availability_zone = var.availability_zones[1]
 }
 
 resource "aws_subnet" "public_web_a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.35.128.0/18"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[0]
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = true
+
+  assign_ipv6_address_on_creation = true
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 2)
 }
 
 resource "aws_subnet" "public_web_b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.35.192.0/18"
-}
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[1]
+  availability_zone       = var.availability_zones[1]
+  map_public_ip_on_launch = true
 
-# INTERNET GATEWAY
+  assign_ipv6_address_on_creation = true
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 3)
+}
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
-
-# ROUTE TABLE
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
@@ -52,10 +59,23 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 }
 
-# ROUTE TABLE ASSOCIATIONS
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_data_a.id
+  route_table_id = aws_route_table.private.id
+}
 
-resource "aws_route_table_association" "a" {
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_data_b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public_a" {
   subnet_id      = aws_subnet.public_web_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_web_b.id
   route_table_id = aws_route_table.public.id
 }
 
