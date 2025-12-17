d0ef3c90793dd647ba85d2f8e995c99be3f8f9a9
Scarus <41557299+Eduard20CR@users.noreply.github.com>
2025-12-16 19:56:22 -0600

    Update subnet configurations to use count for dynamic creation and add new variables for subnet management

--- variables.tf
+++ variables.tf
@@ -1,9 +1,7 @@
-
-
 variable "availability_zones" {
   description = "List of availability zones to use for the subnets"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
 }
 
 variable "cidr_block" {
@@ -22,4 +20,10 @@ variable "public_subnet_cidrs" {
   description = "List of CIDR blocks for public subnets"
   type        = list(string)
   default     = ["10.35.128.0/18", "10.35.192.0/18"]
+}
+
+variable "amount_subnets" {
+  description = "Number of subnets to create in each availability zone"
+  type        = number
+  default     = 2
 }
--- vpc.tf
+++ vpc.tf
@@ -5,36 +5,24 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames             = true
 }
 
-resource "aws_subnet" "private_data_a" {
+resource "aws_subnet" "private_data" {
+  count = var.amount_subnets
+
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidrs[0]
-  availability_zone = var.availability_zones[0]
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
 }
 
-resource "aws_subnet" "private_data_b" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidrs[1]
-  availability_zone = var.availability_zones[1]
-}
+resource "aws_subnet" "public_web" {
+  count = var.amount_subnets
 
-resource "aws_subnet" "public_web_a" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidrs[0]
-  availability_zone       = var.availability_zones[0]
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
 
   assign_ipv6_address_on_creation = true
-  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 2)
-}
-
-resource "aws_subnet" "public_web_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.public_subnet_cidrs[1]
-  availability_zone       = var.availability_zones[1]
-  map_public_ip_on_launch = true
-
-  assign_ipv6_address_on_creation = true
-  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, 3)
+  ipv6_cidr_block                 = cidrsubnet(aws_vpc.main.ipv6_cidr_block, 8, count.index)
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -59,23 +47,16 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route_table_association" "private_a" {
-  subnet_id      = aws_subnet.private_data_a.id
+resource "aws_route_table_association" "private" {
+  count = var.amount_subnets
+  subnet_id      = aws_subnet.private_data[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
-resource "aws_route_table_association" "private_b" {
-  subnet_id      = aws_subnet.private_data_b.id
-  route_table_id = aws_route_table.private.id
-}
-
-resource "aws_route_table_association" "public_a" {
-  subnet_id      = aws_subnet.public_web_a.id
+resource "aws_route_table_association" "public" {
+  count = var.amount_subnets
+  subnet_id      = aws_subnet.public_web[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.public_web_b.id
-  route_table_id = aws_route_table.public.id
-}
 
